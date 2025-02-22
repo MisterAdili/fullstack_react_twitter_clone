cc0b06b7727d4303ed236101732f31c02105a5ae
Jacob Adili <jadili@yahoo.com>
2025-02-22 12:28:45 -0600

    first hello react

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,21 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'homepage#index'
+  get '/feeds' => 'feeds#index'
+
+  # USERS
+  post '/users'                  => 'users#create'
+
+  # SESSIONS
+  post '/sessions'               => 'sessions#create'
+  get  '/authenticated'          => 'sessions#authenticated'
+  delete '/sessions'             => 'sessions#destroy'
+
+  # TWEETS
+  post '/tweets'                 => 'tweets#create'
+  get  '/tweets'                 => 'tweets#index'
+  delete '/tweets/:id'           => 'tweets#destroy'
+  get '/users/:username/tweets' => 'tweets#index_by_user'
+
+  # Redirect all other paths to index page, which will be taken over by AngularJS
+  # get '*path'    => 'homepage#index'
 end
